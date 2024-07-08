6bb06d6ca6a5583451be2ca7e3986def80c02f04
machtheg <thayu.ndegwa@strathmore.edu>
2024-07-08 13:39:24 +0300

    Macharia commit 1:
    Addition of the header files.
    
    They include:
    1. <iostream>
    2. <vector>
    3. <string>
    4. <fstream>
    5. <sstream>
    
    The commit also includes comments on why the header tags were used.

---
+++ main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by machtheg on 7/8/24.
+//Programming project BBIT 1st Sem 2024
+
+#include <iostream> // used to enter input into variables, as well as output those inputs to the user
+#include <vector> // used to improve the arrays. Provides access to the vector keyword. In addition, stores elements in a linear arrangement. Allows fast access to any random element.
+#include <string> // used for storing stings. These are names, clubs, and sports. Without this no string values would be able to be stored.
+#include <fstream> // this is the file stream. It allows us to store the input into the .csv files.
+#include <sstream> // this is the string stream. Used in performing the input and output of the string values.
